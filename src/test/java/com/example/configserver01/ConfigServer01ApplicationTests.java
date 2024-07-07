23fc23df251a3a18721d46cc703c4109a14d1a59
seize <cz2123zc@gmail.com>
2024-07-07 18:00:47 +0900

    깃 컨피그 주소 추가

---
+++ src/test/java/com/example/configserver01/ConfigServer01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserver01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServer01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
